fc8e1c37f17ff76a0cc73088c2907d8d901fe2ab
Kirk <kirk@broadhur.st>
2017-07-17 13:55:07 -0500

    more ng stuff `

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,7 +8,8 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
 	private heroesUrl = 'api/heroes';
-
+	private headers = new Headers({'Content-Type': 'application/json'});
+	
 	constructor(private http: Http) { }
 
 	getHeroes(): Promise<Hero[]> {
@@ -25,9 +26,9 @@ export class HeroService {
 
 	getHeroesSlowly(): Promise<Hero[]> {
   		return new Promise(resolve => {
-		// Simulate server latency with 2 second delay
-    	setTimeout(() => resolve(this.getHeroes()), 2000);
-  	});
+			// Simulate server latency with 2 second delay
+	    	setTimeout(() => resolve(this.getHeroes()), 2000);
+	  	});
 	}
 
 	getHero(id: number) : Promise<Hero> {
@@ -36,5 +37,30 @@ export class HeroService {
 			.toPromise()
 			.then(response => response.json().data as Hero)
 			.catch(this.handleError);
+	}
+
+	save(hero: Hero) : Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+		return this.http
+			.put(url, JSON.stringify(hero), {headers: this.headers})
+			.toPromise()
+			.then(() => hero)
+			.catch(this.handleError);
+	}
+
+	create(name: string) : Promise<Hero> {
+		return this.http
+		  .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+		  .toPromise()
+		  .then(res => res.json().data as Hero)
+		  .catch(this.handleError);
+	}
+
+	delete(id: number) : Promise<void> {
+		const url = `${this.heroesUrl}/${id}`;
+		return this.http.delete(url, {headers: this.headers})
+		  .toPromise()
+		  .then(() => null)
+		  .catch(this.handleError);
 	}
 }
